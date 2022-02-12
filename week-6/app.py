817d320e273fb94ffa1670e08db1213d16fc9ed6
kristy chao <kristy334420@gmail.com>
2022-02-13 02:21:39 +0800

    use where

--- week-6/app.py
+++ week-6/app.py
@@ -60,29 +60,63 @@ def signUp():
     newuserpsd=request.form["pwd"]
     print("註冊使用者輸入:",newusername,newuser,newuserpsd)
    
-    userdblist=""
-    mycursor.execute("SELECT user FROM Member")
-    selectuser=mycursor.fetchall()
-    # print("existuser的結果:",existuser)
-    for userdb in selectuser:
-        userdb=userdb[0]
-        userdblist=userdblist+" "+userdb
-        print("userdb結果:",userdblist,"類型:",type(userdblist))
-
+    
+    
     if (newuser != "" and newuserpsd != "" and newusername != ""):
-        
-        if newuser in userdblist:
-            print("資料庫裡有:newuser")
-            return redirect("/error?message=帳號已經被註冊")
+        mycursor.execute("SELECT user FROM Member WHERE user='"+newuser+"'")
+        checkuser=mycursor.fetchall()
+        print("checkuser是:",checkuser,"資料:",type(checkuser),len(checkuser))
+        if len(checkuser) != 0: # db裡面有資料
+            for i in checkuser:
+                print("i是:",i,"資料:",type(i))
+                print("i[0]是:",i[0],"資料:",type(i[0]))
+                if newuser == i[0]: # 有資料且有符合
+                    print("資料庫裡有:newuser")
+                    return redirect("/error?message=帳號已經被註冊")
 
-        else:
+                else: # 有資料但不符合 newuser
+                    mycursor.execute("INSERT INTO Member (name, user, password) VALUES (%s,%s,%s)",(newusername, newuser, newuserpsd))
+                    mydb.commit()
+                    print("已將 user: "+newuser+"資料存入資料庫")
+                    return redirect ("/")
+        else: # db裡面沒有資料
             mycursor.execute("INSERT INTO Member (name, user, password) VALUES (%s,%s,%s)",(newusername, newuser, newuserpsd))
             mydb.commit()
             print("已將 user: "+newuser+"資料存入資料庫")
             return redirect ("/")
+
     else:
         return redirect("/error?message=有空白欄位，請重新輸入註冊資料")
-  
+
+
+    # ================================================================
+    # ===========================舊程式碼==============================
+    
+    # userdblist=""
+    # mycursor.execute("SELECT user FROM Member")
+    # selectuser=mycursor.fetchall()
+    # # print("existuser的結果:",existuser)
+    # for userdb in selectuser:
+    #     userdb=userdb[0]
+    #     userdblist=userdblist+" "+userdb
+    #     print("userdb結果:",userdblist,"類型:",type(userdblist))
+
+    # if (newuser != "" and newuserpsd != "" and newusername != ""):
+        
+    #     if newuser in userdblist:
+    #         print("資料庫裡有:newuser")
+    #         return redirect("/error?message=帳號已經被註冊")
+
+    #     else:
+    #         mycursor.execute("INSERT INTO Member (name, user, password) VALUES (%s,%s,%s)",(newusername, newuser, newuserpsd))
+    #         mydb.commit()
+    #         print("已將 user: "+newuser+"資料存入資料庫")
+    #         return redirect ("/")
+    # else:
+    #     return redirect("/error?message=有空白欄位，請重新輸入註冊資料")
+    
+    # ================================================================
+    # ===========================舊程式碼end===========================
 
 
 # 使用POST方法連線，建立路徑 "/signin" 對應的處理函式
@@ -93,32 +127,83 @@ def signIn():
 
 #  WHERE user="+"'"+userlogin+"'"
 #  WHERE user=+"'"+userlogin+"'"
-    target="SELECT user, password FROM Member"
+    target="SELECT user, password FROM Member WHERE user="+"'"+userlogin+"'"
     mycursor.execute(target)
-    existuser=mycursor.fetchall()
-    list=()
-    for n in existuser:
-        print("n:",n)
-        list=list+n
-    print("list:",list,'list len:',len(list))
-
+    checklogin=mycursor.fetchall()
     print("登入使用者輸入:",userlogin,psdlogin)
-    for i in range (0,(len(list)//2)):
-        dbuser=list[2*i]
-        dbpwd=list[2*i+1]
-        print("db帳號:",dbuser,"db密碼:",dbpwd)
-        if (dbuser == userlogin and dbpwd == psdlogin):
-            print(list[2*i]+"帳密符合")
-            session["user"]=userlogin
-            result=redirect("/member?name="+userlogin)
-            break
-        else:
-            print(list[2*i]+"帳密不符合")
-            result=redirect("/error?message=帳號或密碼輸入錯誤")
-            continue
+    print("checklogin是:",checklogin,"資料:",type(checklogin),len(checklogin))
+     
+    if len(checklogin) != 0: # db裡面有資料
+        for p in checklogin:
+            if p[0] == userlogin and p[1] ==psdlogin:
+                print("帳密符合")
+                session["user"]=userlogin
+                result=redirect("/member?name="+userlogin)
+                break
+        
+            else:
+                print("帳密不符合")
+                result=redirect("/error?message=帳號或密碼輸入錯誤")
+                continue
+
+        return result
+
+    else: # db裡面沒資料
+        result=redirect("/error?message=帳號或密碼輸入錯誤")
     return result
 
-                
+
+    # list=()
+    # for n in existuser:
+    #     print("n:",n)
+    #     list=list+n
+    # print("list:",list,'list len:',len(list))
+
+    # for i in range (0,(len(list)//2)):
+    #     dbuser=list[2*i]
+    #     dbpwd=list[2*i+1]
+    #     print("db帳號:",dbuser,"db密碼:",dbpwd)
+    #     if (dbuser == userlogin and dbpwd == psdlogin):
+    #         print(list[2*i]+"帳密符合")
+    #         session["user"]=userlogin
+    #         result=redirect("/member?name="+userlogin)
+    #         break
+    #     else:
+    #         print(list[2*i]+"帳密不符合")
+    #         result=redirect("/error?message=帳號或密碼輸入錯誤")
+    #         continue
+    # return result
+
+# ================================================================
+# ===========================舊程式碼==============================
+
+#  target="SELECT user, password FROM Member"
+#     mycursor.execute(target)
+#     existuser=mycursor.fetchall()
+#     list=()
+#     for n in existuser:
+#         print("n:",n)
+#         list=list+n
+#     print("list:",list,'list len:',len(list))
+
+#     print("登入使用者輸入:",userlogin,psdlogin)
+#     for i in range (0,(len(list)//2)):
+#         dbuser=list[2*i]
+#         dbpwd=list[2*i+1]
+#         print("db帳號:",dbuser,"db密碼:",dbpwd)
+#         if (dbuser == userlogin and dbpwd == psdlogin):
+#             print(list[2*i]+"帳密符合")
+#             session["user"]=userlogin
+#             result=redirect("/member?name="+userlogin)
+#             break
+#         else:
+#             print(list[2*i]+"帳密不符合")
+#             result=redirect("/error?message=帳號或密碼輸入錯誤")
+#             continue
+#     return result
+
+# ================================================================
+# ===========================舊程式碼end===========================
 
 # 建立路徑 "/error" 對應的處理函式
 @app.route("/error")
